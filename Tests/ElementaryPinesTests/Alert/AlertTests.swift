c5c09c1794f133f78912df56c8b33b4484aad1fe
amirsaam <amirkamangar@icloud.com>
2026-06-25 19:37:55 +0330

    test(alert): add severity and icon-override variant snapshot tests

--- Tests/ElementaryPinesTests/Alert/AlertTests.swift
+++ Tests/ElementaryPinesTests/Alert/AlertTests.swift
@@ -18,4 +18,88 @@ final class AlertTests: XCTestCase {
             expected
         )
     }
+
+    func testInfoAlert() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-info.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.info) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
+
+    func testSuccessAlert() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-success.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.success) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
+
+    func testWarningAlert() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-warning.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.warning) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
+
+    func testDangerAlert() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-danger.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.danger) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
+
+    func testAlertCustomIcon() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-custom-icon.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.info, icon: .custom(.warning)) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
+
+    func testAlertNoIcon() throws {
+        let expected = try String(
+            contentsOf: fixtureURL("alert-no-icon.html"),
+            encoding: .utf8
+        )
+        HTMLAssertEqual(
+            pinesAlert(.info, icon: .none) {
+                h5 { "Title" }
+                p { "Body" }
+            },
+            expected
+        )
+    }
 }
